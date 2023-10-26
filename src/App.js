5c8ae973e3d779b41424e4de6a18a87c1a6ea819
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-26 09:53:32 +0300

    Initial release

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ToDo from "./ToDo";
+
+function App() {
+  const toDo = {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  };
+
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/ToDo.js
@@ -0,0 +1,16 @@
+function ToDo() {
+  // Railij, kā uztaisīt
+  // te stāvokļa mainīgo?
+  // Railij, atsūti man saiti uz savu
+  // github projektu iekš instagram
+  return (
+    <>
+      <p>UserId: 1</p>
+      <p>Id: 1</p>
+      <p>delectus aut autem</p>
+      <input type="checkbox"></input>
+    </>
+  );
+}
+
+export default ToDo;
