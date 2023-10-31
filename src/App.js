ca662364f95128313180d1edac945aef21f4da75
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-10-31 09:53:29 +0200

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import ToDo from "./ToDo";
 
 function App() {
-  const toDo = {
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
-  };
+  // Uztaisi stāvokļa mainīgo toDo
+  const [toDo, setToDo] = useState({});
+
+  useEffect(() => {
+    async function getToDo() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/4"
+      );
+      const data = await response.json();
+      setToDo(data);
+    }
+    getToDo();
+  }, []);
+
+  // const toDo = {
+  //   userId: 1,
+  //   id: 1,
+  //   title: "delectus aut autem",
+  //   completed: false,
+  // };
 
   return (
     <>
